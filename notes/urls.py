de5e5749989bbd65e5623be89f0e7913670db165
rodney <rramdas@gmail.com>
2024-01-30 20:49:35 +0100

    reset

--- notes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import NoteListView, NoteCreateView, NoteUpdateView, NoteDeleteView
-
-urlpatterns = [
-    path('', NoteListView.as_view(), name='note_list'),
-    path('note/new/', NoteCreateView.as_view(), name='note_new'),
-    path('note/<int:pk>/edit/', NoteUpdateView.as_view(), name='note_edit'),
-    path('note/<int:pk>/delete/', NoteDeleteView.as_view(), name='note_delete'),
-]
--- zettlekasten/urls.py
+++
@@ -1,8 +0,0 @@
-# zettlekasten/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('notes/', include('notes.urls')),  # Include the notes app URLs
-]
