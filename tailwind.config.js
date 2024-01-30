ee045450dad3c861c27724096a578bec3f35326b
rodney <rramdas@gmail.com>
2024-01-30 18:26:15 +0100

    first stab

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './static/**/*.js',
+    // Add other paths where you use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ zettlekasten/urls.py
@@ -0,0 +1,8 @@
+# zettlekasten/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('notes/', include('notes.urls')),  # Include the notes app URLs
+]
